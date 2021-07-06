a4fcad22d28544efaeb37d9852d2555f5ee038a5
rata854 <nitiham2006@gmail.com>
2021-07-06 07:31:19 +0000

    [Add]store_commnet_view

--- app/controllers/store_comments_controller.rb
+++ app/controllers/store_comments_controller.rb
@@ -1,23 +1,52 @@
 class StoreCommentsController < ApplicationController
-  
+
   def show
+    @store = Store.find(params[:store_id])
+    @store_comment = StoreComment.find(params[:id])
   end
-  
+
   def new
+    @store_comment = StoreComment.new
   end
-  
+
   def create
+    @store = Store.find(params[:store_id])
+    @store_comment = StoreComment.new(store_comment_params)
+    @store_comment.user_id = current_user.id
+    @store_comment.store_id = @store.id
+    if @store_comment.save
+      redirect_to store_store_comment_path(id: @store_comment)
+    else
+      render :new
+    end
   end
-  
+
   def edit
+    @store = Store.find(params[:store_id])
+    @store_comment = StoreComment.find(params[:id])
   end
-  
-  def updated
+
+  def update
+    @store_comment = StoreComment.find(params[:id])
+    if @store_comment.update(store_comment_params)
+      redirect_to store_store_comment_path(id: @store_comment)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
+    @store = Store.find(params[:store_id])
+    @store_comment = @store.store_comments.find(params[:id])
+    @store_comment.destroy
+    redirect_to user_path(current_user)
   end
-  
+
   private
-  
+
+  def store_comment_params
+    params.require(:store_comment).permit(:title, :introduction, :product_image,
+                                          :evaluation, :genre, :store_id )
+  end
+
 end
