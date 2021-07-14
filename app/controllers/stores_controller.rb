8ed93c354eedd9ebdb78ac1c4950544e24d67343
rata854 <nitiham2006@gmail.com>
2021-07-14 04:57:36 +0000

    [Add]検索機能

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -34,51 +34,53 @@ class StoresController < ApplicationController
       render :edit
     end
   end
-  
+
   def search
-    # selection = params[:keyword]
-    # @stores = Store.sort(selection)
+    selection = params[:keyword]
+    @stores = Store.sort(selection)
     @genre = params[:genre]
-    if @genre == 'しょうゆ'
-      @soy_sauce_ranks = Store.left_joins(:store_comments).distinct.sort_by do |soy_sauce_rank|
-                          ranks = soy_sauce_rank.store_comments.where(genre: 0)
-                          if ranks.present?
-                            ranks.map(&:rate).sum / ranks.size
-                          else
-                            0
-                          end
-                        end.reverse
-    elsif @genre == 'みそ'
-      @miso_ranks = Store.left_joins(:store_comments).distinct.sort_by do |miso_rank|
-                          ranks = miso_rank.store_comments.where(genre: 1)
-                          if ranks.present?
-                            ranks.map(&:rate).sum / ranks.size
-                          else
-                            0
-                          end
-                        end.reverse
-    elsif @genre == 'とんこつ'
-      @tonkotsu_ranks = Store.left_joins(:store_comments).distinct.sort_by do |tonkotsu_rank|
-                          ranks = tonkotsu_rank.store_comments.where(genre: 2)
-                          if ranks.present?
-                            ranks.map(&:rate).sum / ranks.size
-                          else
-                            0
-                          end
-                        end.reverse
-    elsif @genre == 'しお'
-      @salt_ranks = Store.left_joins(:store_comments).distinct.sort_by do |salt_rank|
-                          ranks = salt_rank.store_comments.where(genre: 3)
-                          if ranks.present?
-                            ranks.map(&:rate).sum / ranks.size
-                          else
-                            0
-                          end
-                        end.reverse
-    else
-      @stores = Store.all
-    end
-    # binding.pry
+    
+    # if @genre == 'しょうゆ'
+    # if params[:genre]
+      # @soy_sauce_ranks = Store.left_joins(:store_comments).distinct.sort_by do |soy_sauce_rank|
+      #                     ranks = soy_sauce_rank.store_comments.where(genre: 0)
+      #                     if ranks.present?
+      #                       ranks.map(&:rate).sum / ranks.size
+      #                     else
+      #                       0
+      #                     end
+      #                   end.reverse
+    # elsif @genre == 'みそ'
+      # @miso_ranks = Store.left_joins(:store_comments).distinct.sort_by do |miso_rank|
+      #                     ranks = miso_rank.store_comments.where(genre: 1)
+      #                     if ranks.present?
+      #                       ranks.map(&:rate).sum / ranks.size
+      #                     else
+      #                       0
+      #                     end
+      #                   end.reverse
+    # elsif @genre == 'とんこつ'
+      # @tonkotsu_ranks = Store.left_joins(:store_comments).distinct.sort_by do |tonkotsu_rank|
+      #                     ranks = tonkotsu_rank.store_comments.where(genre: 2)
+      #                     if ranks.present?
+      #                       ranks.map(&:rate).sum / ranks.size
+      #                     else
+      #                       0
+      #                     end
+      #                   end.reverse
+    # elsif @genre == 'しお'
+      # @salt_ranks = Store.left_joins(:store_comments).distinct.sort_by do |salt_rank|
+      #                     ranks = salt_rank.store_comments.where(genre: 3)
+      #                     if ranks.present?
+      #                       ranks.map(&:rate).sum / ranks.size
+      #                     else
+      #                       0
+      #                     end
+      #                   end.reverse
+    # else
+    #   @stores = Store.all
+    # end
+
   end
 
   private
