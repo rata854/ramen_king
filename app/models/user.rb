d93d14283f9251e995d2b9d3e75df9f9fcdf55b8
rata854 <nitiham2006@gmail.com>
2021-07-17 10:03:12 +0000

    [Add]userモデルテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :stores
   has_many :store_comments, dependent: :destroy
   attachment :profile_image
-  
-  validates :name, length: {maximum: 15, minimum: 2}, uniqueness: true
+
+  validates :name, presence: true, length: {maximum: 15, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 100}
-  
+
+
+  def plus_one(int)
+    int + 1
+  end
+end
+
+context 'n is 1' do
+  it 'return 2' do
+    expect(User.new.plus_one(1)). to eq 2
+  end
+end
+
+context 'n is 2' do
+  it 'return 3' do
+    expect(User.new.plus_one(2)). to eq 3
+  end
 end
