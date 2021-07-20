ff8bf052606e5bed3f980a1d8bd73424dc908162
rata854 <nitiham2006@gmail.com>
2021-07-20 10:18:37 +0000

    [update]main

---
+++ spec/system/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# RSpec.describe User, "Userログインのテスト", type: :system do
+  describe 'Userログインのテスト' do
+  
+    # before do
+    #   @user = FactoryBot.create(:user)
+    # end
+    
+  describe 'トップ画面のテスト' do
+      visit root_path
+  end
+    
+  # end
+end  
