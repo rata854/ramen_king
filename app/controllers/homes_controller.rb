92c52ee227ad9fd51debdde6db3d7837ce664894
rata854 <nitiham2006@gmail.com>
2021-07-29 02:34:10 +0000

    [Update]googlemap

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,11 +2,11 @@ class HomesController < ApplicationController
   
   def top
     # ranksで平均を計算
-    @stores = Store.ranks
+    stores = Store.ranks
     # トップ3用
-    @top_ranks = @stores.first(3)
-    # トップ3以降のランキング
-    @ranks = Kaminari.paginate_array(@stores).page(params[:page]).per(7).offset(3)
+    @top_ranks = stores.first(3)
+    # 4位から10位までのランキング
+    @ranks = Kaminari.paginate_array(stores).page(params[:page]).per(7).offset(3)
   end
 
   def about
