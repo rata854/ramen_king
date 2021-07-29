66b8061fa1669db98607cb51139e9446bc401936
rata854 <nitiham2006@gmail.com>
2021-07-29 05:25:36 +0000

    [Update]geocoder

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,7 @@ class HomesController < ApplicationController
   def top
     # ranksで平均を計算
     stores = Store.ranks
-    # トップ3用
+    # トップ3用ランキング
     @top_ranks = stores.first(3)
     # 4位から10位までのランキング
     @ranks = Kaminari.paginate_array(stores).page(params[:page]).per(7).offset(3)
